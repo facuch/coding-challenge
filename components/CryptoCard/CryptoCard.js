178c026deda27d9a9c24f29ddbda27ce0bf690f0
facuch <fchomczyk@gmail.com>
2022-06-03 17:34:31 -0300

    General styling

--- components/CryptoCard/CryptoCard.js
+++ components/CryptoCard/CryptoCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Text, Dimensions } from 'react-native'
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/Entypo'
+import IconArrow from 'react-native-vector-icons/Feather'
 
 const CryptoCard = () => {
     return(
@@ -8,12 +10,25 @@ const CryptoCard = () => {
                 <Text>Bitcoin</Text>
                 <Text>BTC</Text>
             </View>
-            <View>
-                <Text>$3000</Text>
-                <View >
-                    <Text>!</Text>
-                    <Text>1,82%</Text>
+            <View style={styles.rightContainer}>
+                <View>
+                    <Text>$3000</Text>
+                    <View style={styles.percentage}>
+                        <IconArrow
+                            name={true ? "arrow-up-right" : "arrow-down-left"}
+                            size={15}
+                            color={true ? "green" : "red"}
+                        />
+                        <Text>1,82%</Text>
+                    </View>
                 </View>
+                <TouchableOpacity>
+                    <Icon
+                        name="cross"
+                        size={20}
+                        style={styles.iconStyle}
+                    />
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -28,6 +43,17 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignItems:'center',
         width:Dimensions.get('screen').width - 100
+    },
+    rightContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    iconStyle:{
+        marginLeft:10
+    },
+    percentage:{
+        flexDirection:'row'
     }
 })
 
